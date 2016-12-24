e84dea3faff60a65ef90e745e461f220daacd67d
wenkuanru <wkrjob@163.om>
2016-12-24 21:40:51 +0800

    first commit

---
+++ zipkincallee/src/main/java/com/example/ZipkincalleeApplication.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ZipkincalleeApplication {
+
+	private static final Logger LOG = Logger.getLogger(ZipkincalleeApplication.class.getName());
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkincalleeApplication.class, args);
+	}
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/bottom")
+    public String callHome(){
+        LOG.log(Level.INFO, "Bottom will be called in Callee!");
+        return restTemplate.getForObject("http://localhost:8070", String.class);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
+
+}
---
+++ zipkincaller/src/main/java/com/example/ZipkincallerApplication.java
@@ -0,0 +1,50 @@
+
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ZipkincallerApplication {
+
+    private static final Logger LOG = Logger.getLogger(ZipkincallerApplication.class.getName());
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkincallerApplication.class, args);
+	}
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/")
+    public String callHome(){
+        LOG.log(Level.INFO, "Callee will be called in Caller!");
+        return restTemplate.getForObject("http://localhost:8090/bottom", String.class);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
+
+}
+
+
+
+
+
